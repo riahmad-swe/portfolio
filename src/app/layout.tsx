08da38c44a49ddd669ee79757bfa7949c800dbc4
Rakibul Islam Ahmad <riahmad.swe@gmail.com>
2026-06-27 18:15:22 +0600

    feat: integrated vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Cause } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 import SmoothScrollProvider from "@/components/SmoothScrollProvider";
 import { ScrollToTop } from "@/components/ScrollToTop";
 
@@ -38,6 +39,7 @@ export default function RootLayout({
 					<Footer />
 					<ScrollToTop />
 				</SmoothScrollProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
